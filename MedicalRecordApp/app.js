d1aed3701ad68bc92d6af3cb3b42578f3ebe8f7e
E590 <E590@E590>
2025-10-29 20:52:26 +0300

    _db

--- MedicalRecordApp/app.js
+++ MedicalRecordApp/app.js
@@ -4,5 +4,5 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root', // Your MySQL username
     password: 'root', // Your MySQL root password
-    database: 'medrecord' // Your database name
+    database: 'medrecord_db' // Your database name
 });
